0b04bf07bce2c3fa83ce2ec7247a1b505e1179c5
hongch <honghw0926@naver.com>
2023-12-16 23:54:23 +0900

    저번 코드랑 합치기

--- IoT-PillEat/iot.py
+++ IoT-PillEat/iot.py
@@ -2,13 +2,59 @@ import json
 import asyncio
 import websockets
 import RPi.GPIO as GPIO
+from gpiozero import Robot
+import threading
+import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(16, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT) # LED
+GPIO.setup(25, GPIO.OUT) # 부저
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PID_DOWN) # 버튼 눌리면 on
+GPIO.setup(23, GPIO.OUT) # Trig = 23 초음파 신호 전송핀
+GPIO.setup(24, GPIO.IN)  # Echo = 24 초음파 수신하는 수신 핀
+
+def led_on(): # LED 켜는 함수 생성
+    try:
+        GPIO.output(16, True)
+    finally:
+        GPIO.output(16, False)
+        GPIO.cleanup(16)
+       
+ledOn_Thread = threading.Thread(target = led_on) # led_on 스레드 생성
+
+def buzzer_on(): # 부저 울리는 함수 생성
+    try:
+         pwm = GPIO.pwm(25, 262)
+         pwm.start(50.0)
+    finally:
+        pwm.stop()
+        GPIO.cleanup(25)
+        
+buzzerOn_Thred = threading.Thread(target = buzzer_on) # buzzer_on 스레드 생성
 
 def on_message(message):
     print(f"Received message: {message}")
-
+    
+def measure_distance(): # 초음파로 거리 측정하는 함수 생성
+    try:
+        GPIO.output(23, GPIO.LOW)         
+        time.sleep(0.1)
+        GPIO.output(23, GPIO.HIGH) # trig에서 신호를 내보냄
+        time.sleep(0.00001)          
+        GPIO.output(23, GPIO.LOW)  
+        while GPIO.input(24) == 0: # 24번 핀이 OFF 되는 시점을 시작 시간으로 설정
+            start = time.time()
+            
+        while GPIO.input(24) == 1: # 24번 핀이 다시 ON 되는 시점을 수신 시간으로 설정
+            stop = time.time()
+            
+        time_interval = stop - start # 초음파가 수신되는 시간으로 거리를 계산
+        m_distance = time_interval * 34300 / 2
+        m_distance = round(m_distance, 2)
+        return m_distance
+    finally:
+        pass
+    
 async def on_open(ws):
     login_message = {
         "type": "raspberryLogin",
